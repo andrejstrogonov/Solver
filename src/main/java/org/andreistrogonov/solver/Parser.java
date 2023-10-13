2304beb6efd32ffe67ccd9556316dac25291303a
IValerieI <val.samarchenko@gmail.com>
2023-10-13 12:42:49 +0300

    Added tests 2-7 from file Health

--- src/main/java/org/andreistrogonov/solver/Parser.java
+++ src/main/java/org/andreistrogonov/solver/Parser.java
@@ -5,6 +5,46 @@ public class Parser {
         return i >= 50 && normal == "normal" && male == "male";
     }
 
+    public static boolean weightCalcUnderweight(int i, String underweight, String male) {
+        return i < 50 && underweight.equals("underweight") && male.equals("male");
+    }
+
+    public static String coronaryRiskCalcBelow(String belowAvg) {
+        if (belowAvg.equals("below the average")) {
+            return "low";
+        }
+        return "undefined";
+    }
+
+    public static String coronaryRiskCalcAverage(String average) {
+        if (average.equals("average")) {
+            return "low";
+        }
+        return "undefined";
+    }
+
+    public static String checkStart(String start) {
+        if (start.equals("yes")) {
+            return "different";
+        }
+        return "undefined";
+    }
+
+    public static int lifetimeCalcFemale(int age, String female) {
+        if (age <= 25 && female.equals("female")) {
+            return 72;
+        }
+        return -1;
+    }
+
+    public static int lifetimeCalcMale(int age, String male) {
+        if (age <= 25 && male.equals("male")) {
+            return 67;
+        }
+        return -1;
+    }
+
+
 
 
 
--- src/test/java/org/andreistrogonov/solver/MainTest.java
+++ src/test/java/org/andreistrogonov/solver/MainTest.java
@@ -38,6 +38,21 @@ public class MainTest {
     public void testAdd() {
 //        assertTrue(Parser.weightCalculator(50,"normal", "male"));
 
+        // тесты 2-7, тк несколько дней назад указала, что делаю именно их
+        // 2
+        assertTrue(Parser.weightCalcUnderweight(48,"underweight", "male"));
+        // 3
+        assertEquals("low", Parser.coronaryRiskCalcBelow("below the average"));
+        // 4
+        assertEquals("low", Parser.coronaryRiskCalcAverage("below the average"));
+        // 5
+        assertEquals("low", Parser.checkStart("yes"));
+        // 6
+        assertEquals(72, Parser.lifetimeCalcFemale(24, "female"));
+        // 7
+        assertEquals(72, Parser.lifetimeCalcMale(24, "male"));
+
+
         // Привило 5
         assertEquals("other", f5("yes"));
         assertNotEquals("other", f5("no"));
