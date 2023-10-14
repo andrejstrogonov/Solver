af8c0b178541122e035bf3c8a8b7db528c13f97e
Dmitry2004-11 <88559840+Dmitry2004-11@users.noreply.github.com>
2023-10-14 21:57:25 +0300

    Added tests for rules 8, 13-15

--- src/main/java/org/andreistrogonov/solver/Parser.java
+++ src/main/java/org/andreistrogonov/solver/Parser.java
@@ -1,12 +1,35 @@
 package org.andreistrogonov.solver;
 
 public class Parser {
+
     public static boolean weightCalculator(int i, String normal, String male) {
         return i >= 50 && normal == "normal" && male == "male";
     }
 
+    public static int basicLifeExpectancy(int age, String gender) {
+        if (age >= 25 && age < 55 && gender.equals("ж")) {
+            return 67;
+        }
 
+        return 62;  // default value
+    }
 
+    public static String relativeWeight(int weight, String bodyType, String gender) {
 
+        if (weight >= 85 && bodyType.equals("мелкое") && gender.equals("ж")) {
+            return "излишний";
+        }
+
+        if (weight >= 55 && weight < 85 && bodyType.equals("мелкое") && gender.equals("ж")) {
+            return "излишний";
+        }
+
+        if (weight <= 55 && bodyType.equals("крупное")) {
+            return "недостаточный";
+        }
+
+        return "нормальный";  // default value
+
+    }
 
 }
