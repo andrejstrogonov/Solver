4472d4fda9fb5641c806f1851b99f4ed1a6d5aa3
andrejstrogonov <strogonovandrej@hotmail.com>
2023-11-19 18:31:30 +0300

    Correction Test

--- src/test/java.com.andreistrogonov/java/com/andreistrogonov/MainTest.java
+++ src/test/java.com.andreistrogonov/java/com/andreistrogonov/MainTest.java
@@ -12,42 +12,43 @@ public class MainTest {
 
     @Test
     public void testAdd() {
-       assertTrue(Parser.weightCalculator(50,"normal", "male"));
+       assertTrue(Parser.weightCalculator(50,"normal", "male"));}
 
+    @Test
+   public void testLife() {
         // Rule 8
         assertEquals(67, Parser.basicLifeExpectancy(25, "ж"));
         assertEquals(67, Parser.basicLifeExpectancy(54, "ж"));
-
         assertNotEquals(67, Parser.basicLifeExpectancy(23, "ж"));
         assertNotEquals(67, Parser.basicLifeExpectancy(20, "м"));
-        assertNotEquals(67, Parser.basicLifeExpectancy(25, "м"));
-
-        // Rule 13
+        assertNotEquals(67, Parser.basicLifeExpectancy(25, "м"));}
+   @Test
+   public void testRule13(){
         assertEquals("излишний", Parser.relativeWeight(85, "мелкое", "ж"));
         assertEquals("излишний", Parser.relativeWeight(86, "мелкое", "ж"));
-
         assertNotEquals("излишний", Parser.relativeWeight(45, "мелкое", "ж"));
         assertNotEquals("излишний", Parser.relativeWeight(85, "крупное", "ж"));
         assertNotEquals("излишний", Parser.relativeWeight(85, "мелкое", "м"));
         assertNotEquals("излишний", Parser.relativeWeight(45, "крупное", "ж"));
         assertNotEquals("излишний", Parser.relativeWeight(87, "крупное", "м"));
         assertNotEquals("излишний", Parser.relativeWeight(45, "мелкое", "м"));
-        assertNotEquals("излишний", Parser.relativeWeight(45, "крупное", "м"));
+        assertNotEquals("излишний", Parser.relativeWeight(45, "крупное", "м"));}
+ @Test
+ public void testRule14(){
 
-        // Rule 14
         assertEquals("излишний", Parser.relativeWeight(55, "мелкое", "ж"));
         assertEquals("излишний", Parser.relativeWeight(84, "мелкое", "ж"));
         assertEquals("излишний", Parser.relativeWeight(65, "мелкое", "ж"));
-
         assertNotEquals("излишний", Parser.relativeWeight(45, "мелкое", "ж"));
         assertNotEquals("излишний", Parser.relativeWeight(82, "крупное", "ж"));
         assertNotEquals("излишний", Parser.relativeWeight(82, "мелкое", "м"));
         assertNotEquals("излишний", Parser.relativeWeight(45, "крупное", "ж"));
         assertNotEquals("излишний", Parser.relativeWeight(82, "крупное", "м"));
         assertNotEquals("излишний", Parser.relativeWeight(45, "мелкое", "м"));
-        assertNotEquals("излишний", Parser.relativeWeight(45, "крупное", "м"));
+        assertNotEquals("излишний", Parser.relativeWeight(45, "крупное", "м"));}
+ @Test
+ public void testRule15(){
 
-        // Rule 15
         assertEquals("недостаточный", Parser.relativeWeight(55, "крупное", "-"));
         assertEquals("недостаточный", Parser.relativeWeight(45, "крупное", "-"));
         assertEquals("недостаточный", Parser.relativeWeight(45, "крупное", "м"));
@@ -59,8 +60,5 @@ public class MainTest {
         assertNotEquals("недостаточный", Parser.relativeWeight(45, "мелкое", "м"));
         assertNotEquals("недостаточный", Parser.relativeWeight(55, "мелкое", "ж"));
         assertNotEquals("недостаточный", Parser.relativeWeight(56, "крупное", "м"));
-        assertNotEquals("недостаточный", Parser.relativeWeight(56, "крупное", "ж"));
-
-    }
-
+        assertNotEquals("недостаточный", Parser.relativeWeight(56, "крупное", "ж"));}
 }
