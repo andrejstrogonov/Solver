4e52ce3c166c56a920395ef14322ac1c31482125
andrejstrogonov <strogonovandrej@hotmail.com>
2023-11-25 18:36:54 +0300

    Need to refactor knowledge base

--- src/main/java/main/java/com/andreistrogonov/Parser.java
+++ src/main/java/main/java/com/andreistrogonov/Parser.java
@@ -4,19 +4,21 @@ import java.util.Objects;
 
 public class Parser {
 
+
     public static boolean weightCalculator(int i, String normal, String male) {
         return i >= 50 && Objects.equals(normal, "normal") && Objects.equals(male, "male");
     }
 
-    public static int basicLWomanifeExpectancy(String female) {
+    public static int basicLWomanifeExpectancy() {
         return 67;
     }
 
-    public static int basicManLifeExpectancy(String male) {
+    public static int basicManLifeExpectancy() {
         return 67;
     }
 
-    public static String secondWeightCalculator(int i, String small, String female) {
-        return "redundant";
+    public static boolean secondWeightCalculator(int age, String small, String female) {
+       return age>=45 && Objects.equals(small,"small") && Objects.equals(female,"female");
+
     }
 }
--- src/test/java/main/com/andreistrogonov/solver/main/com/andreistrogonov/solver/MainTest.java
+++ src/test/java/main/com/andreistrogonov/solver/main/com/andreistrogonov/solver/MainTest.java
@@ -12,47 +12,10 @@ public class MainTest {
         assertTrue(Parser.weightCalculator(50,"normal", "male"));}
     @Test
     public void testLife(){
-        assertEquals(67, Parser.basicLWomanifeExpectancy("female"));
-        assertEquals(67,Parser.basicManLifeExpectancy("male"));}
+        assertEquals(67, Parser.basicLWomanifeExpectancy());
+        assertEquals(67,Parser.basicManLifeExpectancy());}
     @Test
     public void testRule13() {
-        assertEquals("redundant",Parser.secondWeightCalculator(45,"small","female") );
-        assertEquals("redundant", Parser.relativeWeight(85, "small", "female"));
-        assertEquals("redundant", Parser.relativeWeight(86, "small", "female"));
-        assertNotEquals("redundant", Parser.relativeWeight(45, "small", "female"));
-        assertNotEquals("redundant", Parser.relativeWeight(85, "large", "female"));
-        assertNotEquals("redundant", Parser.relativeWeight(85, "small", "male"));
-        assertNotEquals("redundant", Parser.relativeWeight(45, "large", "female"));
-        assertNotEquals("redundant", Parser.relativeWeight(87, "large", "male"));
-        assertNotEquals("redundant", Parser.relativeWeight(45, "small", "male"));
-        assertNotEquals("redundant", Parser.relativeWeight(45, "large", "male"));
-    }
-    @Test
-    public void testRule14() {
-        assertEquals("redundant", Parser.relativeWeight(55, "small", "female"));
-        assertEquals("redundant", Parser.relativeWeight(84, "small", "female"));
-        assertEquals("redundant", Parser.relativeWeight(65, "small", "female"));
-        assertNotEquals("redundant", Parser.relativeWeight(45, "small", "female"));
-        assertNotEquals("redundant", Parser.relativeWeight(82, "large", "female"));
-        assertNotEquals("redundant", Parser.relativeWeight(82, "small", "male"));
-        assertNotEquals("redundant", Parser.relativeWeight(45, "large", "female"));
-        assertNotEquals("redundant", Parser.relativeWeight(82, "large", "male"));
-        assertNotEquals("redundant", Parser.relativeWeight(45, "small", "male"));
-        assertNotEquals("redundant", Parser.relativeWeight(45, "largeе", "male"));
-    }
-    @Test
-    public void testRule15() {
-        assertEquals("insufficient", Parser.relativeWeight(55, "large", "-"));
-        assertEquals("insufficient", Parser.relativeWeight(45, "large", "-"));
-        assertEquals("insufficient", Parser.relativeWeight(45, "large", "м"));
-        assertEquals("insufficient", Parser.relativeWeight(55, "large", "ж"));
+        assertTrue(Parser.secondWeightCalculator(45,"small","female")); }
 
-        assertNotEquals("insufficient", Parser.relativeWeight(45, "small", "-"));
-        assertNotEquals("insufficient", Parser.relativeWeight(55, "small", "-"));
-        assertNotEquals("insufficient", Parser.relativeWeight(56, "large", "-"));
-        assertNotEquals("insufficient", Parser.relativeWeight(45, "small", "male"));
-        assertNotEquals("insufficient", Parser.relativeWeight(55, "small", "female"));
-        assertNotEquals("insufficient", Parser.relativeWeight(56, "large", "male"));
-        assertNotEquals("insufficient", Parser.relativeWeight(56, "large", "female"));
-    }
 }
