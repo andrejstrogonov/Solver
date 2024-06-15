089f3244113fab01bdb0e7529c3f0d98fcfb6c19
andrejstrogonov <strogonovandrej@hotmail.com>
2024-06-15 21:32:19 +0300

    Corrections

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
