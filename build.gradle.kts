99b691751b1723e6f82a64f2256efa46f64d048e
andrejstrogonov <strogonovandrej@hotmail.com>
2024-08-14 22:56:38 +0300

    Изменение фреймворка мутационного тестирования

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id ("info.solidsoft.pitest") version "1.15.0"
 }
 
 group = "com.andrejstrogonov"
@@ -15,9 +16,20 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:5.12.0")
-    implementation("org.pitest:pitest-parent:1.16.1")
+
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+configure<info.solidsoft.gradle.pitest.PitestPluginExtension> {
+    junit5PluginVersion.set("1.0.0")
+    avoidCallsTo.set(setOf("kotlin.jvm.internal"))
+    mutators.set(setOf("STRONGER"))
+    targetClasses.set(setOf("pitest.test.*"))  //by default "${project.group}.*"
+    targetTests.set(setOf("pitest.test.*Test"))
+//    pitestVersion.set("1.4.0")   //current defined for Gradle plugin PIT version should be used
+    threads.set(Runtime.getRuntime().availableProcessors())
+    outputFormats.set(setOf("XML", "HTML"))
 }
