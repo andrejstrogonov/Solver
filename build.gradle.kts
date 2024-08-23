56be8e57a3492d27ae8590799cf4e919d8d2a8fa
andrejstrogonov <strogonovandrej@hotmail.com>
2024-08-23 22:41:34 +0300

    Подготовка к созданию специальной базы данных (MongoDB в проекте) для ведения логов

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,10 @@ repositories {
 dependencies {
     implementation("org.pitest:pitest:1.16.1")
     implementation("org.pitest:pitest-junit5-plugin:1.2.1")
+    implementation("org.springframework.boot:spring-boot-docker-compose:3.1.1")
+    implementation("org.springframework.boot:spring-boot-starter-parent:3.3.3")
     implementation("log4j:log4j:1.2.17")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:5.12.0")
